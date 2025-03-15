b152dd9d20df4b6d897de075f340628935f959b8
schro the cat <75566420+urs-truly@users.noreply.github.com>
2025-03-15 20:30:35 +0400

    Fixed localized import issue

--- src/app/signup/organization/page.tsx
+++ src/app/signup/organization/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { Input } from "C:\\Users\\augus\\OneDrive\\Documents\\Hacksus\\goworker\\src\\components\\ui\\input";
+import { Input } from "@/components/ui/input";
 import { useRouter } from "next/navigation";
 
 export default function OrganizationSignup() {
